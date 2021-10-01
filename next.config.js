3665c956552cba6c494d74ef45f2dfca9cf068cf
Jerome Escultura <esculturajerome@gmail.com>
2021-10-02 04:43:41 +0800

    Added next config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  target: "serverless",
+};
+
+module.exports = {
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+};
+
+module.exports = {
+  webpack5: false,
+};

b204e41a7bc0a3c73146638a68b736068005913a
Jerome Escultura <esculturajerome@gmail.com>
2021-10-30 22:38:45 +0800

    Made logout button blue

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -73,7 +73,7 @@ function NavBar() {
             ) : (
               <a
                 href=""
-                className="py-2 px-3 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300"
+                className="py-2 px-3 bg-blue-400 hover:bg-blue-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300"
                 onClick={signIn}
               >
                 Login
